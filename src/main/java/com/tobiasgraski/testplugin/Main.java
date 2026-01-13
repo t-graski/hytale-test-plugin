7060cce2efe52215d91852e329ec9935d4ba3f4d
Tobias Graski <57156093+t-graski@users.noreply.github.com>
2026-01-13 18:34:00 +0000

    Initial commit

---
+++ src/main/java/com/tobiasgraski/testplugin/Main.java
@@ -0,0 +1,31 @@
+package com.tobiasgraski.testplugin;
+
+import com.hypixel.hytale.common.plugin.PluginIdentifier;
+import com.hypixel.hytale.server.core.plugin.*;
+import com.tobiasgraski.testplugin.commands.HelloCommand;
+
+public class Main extends JavaPlugin {
+
+    public Main(JavaPluginInit init) {
+        super(init);
+    }
+
+    @Override
+    protected void setup() {
+        registerHandlers();
+        registerHello();
+    }
+
+    @Override
+    protected void shutdown() {
+        super.shutdown();
+    }
+
+    private void registerHandlers() {
+        var plugin = PluginManager.get().getPlugin(new PluginIdentifier("Test", "TestPlugin"));
+    }
+
+    private void registerHello() {
+        getCommandRegistry().registerCommand(new HelloCommand());
+    }
+}

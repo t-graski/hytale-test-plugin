7a8e508904786d51bb81395037601c8e7d8bd752
Arman <7013133+Armannnnnn@users.noreply.github.com>
2026-01-14 11:17:03 -0500

    Add DuelLoadouts class for managing player inventories

---
+++ src/main/java/com/tobiasgraski/testplugin/utils/DuelLoadouts.java
@@ -0,0 +1,37 @@
+package com.tobiasgraski.testplugin.utils;
+
+import com.hypixel.hytale.server.core.entity.entities.Player;
+import com.hypixel.hytale.server.core.inventory.Inventory;
+import com.hypixel.hytale.server.core.inventory.ItemStack;
+
+public final class DuelLoadouts {
+
+    private DuelLoadouts() {}
+
+    public static void applyBasicDuelKit(Player player) {
+        Inventory inv = player.getInventory();
+
+        inv.clear();
+
+        inv.getHotbar().setItemStackForSlot((short) 0, new ItemStack("Weapon_Sword_Iron", 1));
+        inv.getHotbar().setItemStackForSlot((short) 1, new ItemStack("Potion_Health_Greater", 1));
+
+        inv.getArmor().setItemStackForSlot((short) 0, new ItemStack("Armor_Iron_Head", 1));
+        inv.getArmor().setItemStackForSlot((short) 1, new ItemStack("Armor_Iron_Chest", 1));
+        inv.getArmor().setItemStackForSlot((short) 2, new ItemStack("Armor_Iron_Hands", 1));
+        inv.getArmor().setItemStackForSlot((short) 3, new ItemStack("Armor_Iron_Legs", 1));
+
+        inv.setActiveHotbarSlot((byte) 0);
+
+        player.setInventory(inv);
+    }
+    
+
+    public static void clearInventory(Player player) {
+        Inventory inv = player.getInventory();
+        inv.clear();
+
+        player.setInventory(inv);
+    }
+}
+

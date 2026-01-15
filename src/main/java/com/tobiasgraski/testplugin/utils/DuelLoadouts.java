f613ee45fd52c77e1bba8b343279362dfb641e46
Arman <7013133+Armannnnnn@users.noreply.github.com>
2026-01-15 17:57:49 -0500

    Update DuelLoadouts.java

--- src/main/java/com/tobiasgraski/testplugin/utils/DuelLoadouts.java
+++ src/main/java/com/tobiasgraski/testplugin/utils/DuelLoadouts.java
@@ -3,30 +3,47 @@ package com.tobiasgraski.testplugin.utils;
 import com.hypixel.hytale.server.core.entity.entities.Player;
 import com.hypixel.hytale.server.core.inventory.Inventory;
 import com.hypixel.hytale.server.core.inventory.ItemStack;
-import com.hypixel.hytale.server.core.universe.PlayerRef;
+import com.hypixel.hytale.server.core.universe.world.storage.EntityStore;
 
 public final class DuelLoadouts {
 
     private DuelLoadouts() {}
 
     public static void applyBasicDuelKit(Player player) {
-        Inventory inv = player.getInventory();
+        if (player == null) return;
 
-        inv.clear();
+        var ref = player.getReference();
+        var store = ref.getStore();
+        var world = ((EntityStore) store.getExternalData()).getWorld();
+        if (world == null) return;
 
-        inv.getHotbar().setItemStackForSlot((short) 0, new ItemStack("Weapon_Sword_Iron", 1));
-        inv.getHotbar().setItemStackForSlot((short) 1, new ItemStack("Potion_Health_Greater", 1));
+        world.execute(() -> {
+            Inventory inv = player.getInventory();
+            inv.clear();
 
-        inv.getArmor().setItemStackForSlot((short) 0, new ItemStack("Armor_Iron_Head", 1));
-        inv.getArmor().setItemStackForSlot((short) 1, new ItemStack("Armor_Iron_Chest", 1));
-        inv.getArmor().setItemStackForSlot((short) 2, new ItemStack("Armor_Iron_Hands", 1));
-        inv.getArmor().setItemStackForSlot((short) 3, new ItemStack("Armor_Iron_Legs", 1));
+            inv.getHotbar().setItemStackForSlot((short) 0, new ItemStack("Weapon_Sword_Iron", 1));
+            inv.getHotbar().setItemStackForSlot((short) 1, new ItemStack("Potion_Health_Greater", 1));
 
-        inv.setUsingToolsItem(false);
-        inv.setActiveHotbarSlot((byte) 0);
+            inv.getArmor().setItemStackForSlot((short) 0, new ItemStack("Armor_Iron_Head", 1));
+            inv.getArmor().setItemStackForSlot((short) 1, new ItemStack("Armor_Iron_Chest", 1));
+            inv.getArmor().setItemStackForSlot((short) 2, new ItemStack("Armor_Iron_Hands", 1));
+            inv.getArmor().setItemStackForSlot((short) 3, new ItemStack("Armor_Iron_Legs", 1));
 
-        player.sendInventory();
+            inv.setUsingToolsItem(false);
+            inv.setActiveHotbarSlot((byte) 0);
+
+            player.sendInventory();
+
+            player.invalidateEquipmentNetwork();
+            player.getStatModifiersManager().setRecalculate(true);
+
+            world.execute(() -> {
+                DuelStatsUtil.setHealth(player, 9999);
+                DuelStatsUtil.setStamina(player, 9999);
+            });
+        });
     }
+
     
 
     public static void clearInventory(Player player) {
@@ -38,4 +55,4 @@ public final class DuelLoadouts {
         
         player.sendInventory();
     }
-}
+}		
